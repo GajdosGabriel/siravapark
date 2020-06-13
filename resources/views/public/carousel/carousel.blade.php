a1aa50bd01d48a7b2c16d7c47dce86ab97303772
GajdosGabriel <gajdosgabo@gmail.com>
2020-06-13 11:27:22 +0200

    refactoring index page

---
+++ resources/views/public/carousel/carousel.blade.php
@@ -0,0 +1,36 @@
+<div id="homeBlock" class="pageBlock">
+    <div id="carouselMain" class="carousel slide carousel-fade" data-ride="carousel">
+        <div class="carousel-inner">
+
+            {{--  Slide 1--}}
+            <div class="carousel-item active">
+                <img class="d-block d-md-none w-100" src="{{ asset('images/carousel/1sm.jpg') }}" alt="">
+                <img class="d-none d-md-block w-100" src="{{ asset('images/carousel/1.jpg') }}" alt="">
+
+                <div class="carousel-caption">
+                    @include('includes/carousel_caption')
+                </div>
+            </div>
+
+            {{--Slide 2--}}
+            <div class="carousel-item">
+                <img class="d-block d-md-none w-100" src="{{ asset('images/carousel/2sm.jpg') }}" alt="">
+                <img class="d-none d-md-block w-100" src="{{ asset('images/carousel/2.jpg') }}" alt="">
+
+                <div class="carousel-caption">
+                    @include('includes/carousel_caption')
+                </div>
+            </div>
+        </div>
+
+        <a class="carousel-control-prev" href="#carouselMain" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+
+        <a class="carousel-control-next" href="#carouselMain" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
---
+++ resources/views/public/carousel/carousel_caption.blade.php
@@ -0,0 +1,46 @@
+<div class="top text-center" data-aos="fade-right">
+    @include('includes/langs')
+    <img class="mx-auto" style="height: 30vh" src="{{ asset('images/sirava-park-logo.png') }}">
+
+</div>
+
+<div class="bottom" data-aos="fade-left">
+    <div>
+        {{--<p class="text text-shadow">@lang('index.spectacular')</p>--}}
+
+        <a href="" class="btn btn-white" data-toggle="modal" data-target="#holidayFormModal22">@lang('index.free')</a>
+    </div>
+
+    {{--   Social icons--}}
+{{--    <div id="soc" class="row">--}}
+{{--        <div class="col-2 offset-1">--}}
+{{--            <a href="https://www.facebook.com/%C5%A0%C3%ADrava-Park-1421432671327223/" target="_blank">--}}
+{{--                <i class="fab fa-facebook"></i>--}}
+{{--            </a>--}}
+{{--        </div>--}}
+
+{{--        <div class="col-2">--}}
+{{--            <a href="https://www.instagram.com/siravapark/" target="_blank">--}}
+{{--                <i class="fab fa-instagram"></i>--}}
+{{--            </a>--}}
+{{--        </div>--}}
+
+{{--        <div class="col-2">--}}
+{{--            <a href="https://twitter.com/SiravaP" target="_blank">--}}
+{{--                <i class="fab fa-twitter"></i>--}}
+{{--            </a>--}}
+{{--        </div>--}}
+
+{{--        <div class="col-2">--}}
+{{--            <a href="https://www.youtube.com/channel/UCjwf6q-6A0uZQtl1FXXlMiw" target="_blank">--}}
+{{--                <i class="fab fa-youtube"></i>--}}
+{{--            </a>--}}
+{{--        </div>--}}
+
+{{--        <div class="col-2">--}}
+{{--            <a href="https://www.linkedin.com/company/sirava-park/" target="_blank">--}}
+{{--                <i class="fab fa-linkedin"></i>--}}
+{{--            </a>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+</div>
